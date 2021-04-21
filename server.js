b6575cb29fdfa4eaf1537abd737d6e74aedfdf32
Hiro Kumagami <hkumagam@ucsc.edu>
2021-04-20 18:46:51 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req,res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+  var num1 = Number(req.body.maxRoom);
+  var num2 = Number(req.body.minRoom);
+  res.send("Max Rooms: " + num1 + "\nMin Rooms: " + num2 );
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is running on port 3000 mate.");
+});
