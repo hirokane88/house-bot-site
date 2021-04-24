6c209c127607fe4cc23fe8dc8e9db8ca51ea6c47
Hiro Kumagami <hkumagam@ucsc.edu>
2021-04-23 19:25:23 -0700

    nothing

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,13 @@ $("select").addClass("placeholder");
 
 $("select").click(function() {
   $(this).css("color", "black");
+  $("option").css("color", "black");
 });
+
+$("option").click(function() {
+  $(this).parentNode.css("color", "black");
+});
+
 
 $("input").click(function() {
   $(this).css("color", "black");
